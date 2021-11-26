8646ae17ac9d2b613f2d69b7454863987507a808
Tarith <tarithj@gmail.com>
2021-11-26 20:45:13 +0530

    [DOC][ApiSpec]: Added documentation for Scene.SetTileTypeId

--- Cellua/ApiSpec/Scene.lua
+++ Cellua/ApiSpec/Scene.lua
@@ -8,3 +8,9 @@
 ---@param b number Blue value of the tile
 ---@param a number Alpha value of the tile
 function Scene.ChangeColor(x, y, r, g, b, a) end
+
+--- Change type id of the tile
+---@param x number x position of the tile
+---@param y number y position of the tile
+---@param typeid number type id to set
+function Scene.SetTileTypeId(x, y, typeid) end
