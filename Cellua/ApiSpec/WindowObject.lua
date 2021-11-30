3fef8e344b7655ae4c40faee3e17f0e22c1046b6
Tarith <tarithj@gmail.com>
2021-11-30 21:11:38 +0530

    [DOC][ApiSpec]: Updated documentation to match new changes

---
+++ Cellua/ApiSpec/WindowObject.lua
@@ -0,0 +1,31 @@
+﻿---@class WindowObject
+WindowObject = {}
+
+--- Changes the title of the window
+---@param title string
+function WindowObject.SetTitle(title) end
+
+--- Closes the window
+function WindowObject.Close() end
+
+--- Clears the window screen
+function WindowObject.Clear() end
+
+--- Changes the framerate limit
+---@param limit number
+function WindowObject.SetFramerateLimit(limit) end
+
+--- Returns true if the window isOpened
+---@return boolean
+function WindowObject.IsOpen() end
+
+--- Displays the scene
+function WindowObject.Display() end
+
+--- Returns the framerate
+---@return number
+function WindowObject.GetFramerate() end
+
+--- Returns a SceneObject
+---@return SceneObject
+function WindowObject.GetScene() end
